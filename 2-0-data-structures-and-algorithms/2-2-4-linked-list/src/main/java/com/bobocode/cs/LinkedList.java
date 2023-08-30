3f29f505e499f43d37c55a99e11448235a7232b7
Ruslan Hladchenko <rugladchenko@gmail.com>
2023-08-30 16:37:13 +0300

    complete LinkedList

--- 2-0-data-structures-and-algorithms/2-2-4-linked-list/src/main/java/com/bobocode/cs/LinkedList.java
+++ 2-0-data-structures-and-algorithms/2-2-4-linked-list/src/main/java/com/bobocode/cs/LinkedList.java
@@ -1,13 +1,14 @@
 package com.bobocode.cs;
 
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.util.NoSuchElementException;
 
 /**
- * {@link LinkedList} is a list implementation that is based on singly linked generic nodes. A node is implemented as
- * inner static class {@link Node<T>}.
+ * {@link LinkedList} is a list implementation that is based on singly linked generic nodes. A node
+ * is implemented as inner static class {@link Node<T>}.
  * <p><p>
- * <strong>TODO: to get the most out of your learning, <a href="https://www.bobocode.com/learn">visit our website</a></strong>
+ * <strong>TODO: to get the most out of your learning, <a
+ * href="https://www.bobocode.com/learn">visit our website</a></strong>
  * <p>
  *
  * @param <T> generic type parameter
@@ -16,133 +17,254 @@ import com.bobocode.util.ExerciseNotCompletedException;
  */
 public class LinkedList<T> implements List<T> {
 
-    /**
-     * This method creates a list of provided elements
-     *
-     * @param elements elements to add
-     * @param <T>      generic type
-     * @return a new list of elements the were passed as method parameters
-     */
-    public static <T> LinkedList<T> of(T... elements) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  private int size = 0;
 
-    /**
-     * Adds an element to the end of the list.
-     *
-     * @param element element to add
-     */
-    @Override
-    public void add(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Adds a new element to the specific position in the list. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index   an index of new element
-     * @param element element to add
-     */
-    @Override
-    public void add(int index, T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Changes the value of an list element at specific position. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index   an position of element to change
-     * @param element a new element value
-     */
-    @Override
-    public void set(int index, T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Retrieves an elements by its position index. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index element index
-     * @return an element value
-     */
-    @Override
-    public T get(int index) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Returns the first element of the list. Operation is performed in constant time O(1)
-     *
-     * @return the first element of the list
-     * @throws java.util.NoSuchElementException if list is empty
-     */
-    @Override
-    public T getFirst() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Returns the last element of the list. Operation is performed in constant time O(1)
-     *
-     * @return the last element of the list
-     * @throws java.util.NoSuchElementException if list is empty
-     */
-    @Override
-    public T getLast() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
-
-    /**
-     * Removes an elements by its position index. In case provided index in out of the list bounds it
-     * throws {@link IndexOutOfBoundsException}
-     *
-     * @param index element index
-     * @return deleted element
-     */
-    @Override
-    public T remove(int index) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  private Node<T> first;
 
 
-    /**
-     * Checks if a specific exists in he list
-     *
-     * @return {@code true} if element exist, {@code false} otherwise
-     */
-    @Override
-    public boolean contains(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
-    }
+  private Node<T> last;
 
-    /**
-     * Checks if a list is empty
-     *
-     * @return {@code true} if list is empty, {@code false} otherwise
-     */
-    @Override
-    public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * This method creates a list of provided elements
+   *
+   * @param elements elements to add
+   * @param <T>      generic type
+   * @return a new list of elements the were passed as method parameters
+   */
+  public static <T> LinkedList<T> of(T... elements) {
+    LinkedList<T> linkedList = new LinkedList<>();
+    for (T element : elements) {
+      linkedList.add(element);
     }
+    return linkedList;
+  }
 
-    /**
-     * Returns the number of elements in the list
-     *
-     * @return number of elements
-     */
-    @Override
-    public int size() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * Adds an element to the end of the list.
+   *
+   * @param element element to add
+   */
+  @Override
+  public void add(T element) {
+    if (first == null) {
+      first = new Node<>(element);
+      size++;
+    } else {
+      Node<T> current = first;
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = new Node<>(element);
+      last = current.next;
+      size++;
     }
+  }
 
-    /**
-     * Removes all list elements
-     */
-    @Override
-    public void clear() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+  /**
+   * Adds a new element to the specific position in the list. In case provided index in out of the
+   * list bounds it throws {@link IndexOutOfBoundsException}
+   *
+   * @param index   an index of new element
+   * @param element element to add
+   */
+  @Override
+  public void add(int index, T element) {
+    if (index < 0 || index > size) {
+      throw new IndexOutOfBoundsException();
     }
+    Node<T> node = new Node<>(element);
+    if (index == 0) {
+      if (size == 0) {
+        first = node;
+        last = node;
+      } else {
+        node.next = first;
+        first = node;
+      }
+    } else if (index == size) {
+      last.next = node;
+    } else {
+      Node<T> prev = first;
+      Node<T> current = first.next;
+      for (int i = 1; i < size && current != null; i++) {
+        if (index == i) {
+          prev.next = node;
+          node.next = current;
+        }
+        prev = current;
+        current = current.next;
+      }
+    }
+    size++;
+  }
+
+  /**
+   * Changes the value of an list element at specific position. In case provided index in out of the
+   * list bounds it throws {@link IndexOutOfBoundsException}
+   *
+   * @param index   an position of element to change
+   * @param element a new element value
+   */
+  @Override
+  public void set(int index, T element) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException();
+    }
+    Node<T> node = first;
+    for (int i = 0; i < size; i++) {
+      if (index == i) {
+        node.element = element;
+      }
+      node = node.next;
+    }
+  }
+
+  /**
+   * Retrieves an elements by its position index. In case provided index in out of the list bounds
+   * it throws {@link IndexOutOfBoundsException}
+   *
+   * @param index element index
+   * @return an element value
+   */
+  @Override
+  public T get(int index) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException();
+    }
+    Node<T> current = first;
+    for (int i = 0; i < size; i++) {
+      if (index == i) {
+        return current.element;
+      }
+      current = current.next;
+    }
+    return null;
+  }
+
+  /**
+   * Returns the first element of the list. Operation is performed in constant time O(1)
+   *
+   * @return the first element of the list
+   * @throws java.util.NoSuchElementException if list is empty
+   */
+  @Override
+  public T getFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return first.element;
+  }
+
+  /**
+   * Returns the last element of the list. Operation is performed in constant time O(1)
+   *
+   * @return the last element of the list
+   * @throws java.util.NoSuchElementException if list is empty
+   */
+  @Override
+  public T getLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    return last.element;
+  }
+
+  /**
+   * Removes an elements by its position index. In case provided index in out of the list bounds it
+   * throws {@link IndexOutOfBoundsException}
+   *
+   * @param index element index
+   * @return deleted element
+   */
+  @Override
+  public T remove(int index) {
+    if (index < 0 || index > size || size == 0) {
+      throw new IndexOutOfBoundsException();
+    }
+    T removed = null;
+
+    if (size == 1 && index == 0) {
+      removed = first.element;
+      first = null;
+      last = null;
+    } else if (index == 0) {
+      removed = first.element;
+      first = first.next;
+    } else {
+      Node<T> prev = first;
+      Node<T> current = prev.next;
+      for (int i = 1; i < size; i++) {
+        if (index == i) {
+          prev.next = current.next;
+          removed = current.element;
+          if (index == size - 1) {
+            last = prev;
+          }
+          break;
+        }
+        prev = current;
+        current = current.next;
+      }
+    }
+    size--;
+
+    return removed;
+  }
+
+  /**
+   * Checks if a specific exists in he list
+   *
+   * @return {@code true} if element exist, {@code false} otherwise
+   */
+  @Override
+  public boolean contains(T element) {
+    Node<T> current = first;
+    while (current != null) {
+      if (current.element.equals(element)) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  /**
+   * Checks if a list is empty
+   *
+   * @return {@code true} if list is empty, {@code false} otherwise
+   */
+  @Override
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Returns the number of elements in the list
+   *
+   * @return number of elements
+   */
+  @Override
+  public int size() {
+    return size;
+  }
+
+  /**
+   * Removes all list elements
+   */
+  @Override
+  public void clear() {
+    first = null;
+    size = 0;
+  }
+
+  private static class Node<T> {
+
+    T element;
+
+    Node<T> next;
+
+    public Node(T element) {
+      this.element = element;
+    }
+  }
 }
